31dc139ae0be8454d7df18fde932fcb1f24f1e2e
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-04-11 15:55:35 +0700

    feat: create colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+
+const Color kBackground = Color(0xFFFCFCFC);
+const Color kPrimaryLight = Color(0xFFEDF6F3);
+const Color kPrimary = Color(0xFFDAEFE8);
+const Color kPrimaryDark = Color(0xFF88A1AE);
