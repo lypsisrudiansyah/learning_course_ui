c04f5b01c6787423ca710260bd9c204c4723dd37
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-04-11 16:06:31 +0700

    update home_screen.dart

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,18 +1,35 @@
 // ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
+import 'package:learning_course_ui/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      // appBar: AppBar(
-      //   title: const Text("Dashboard"),
-      //   actions: const [],
-      // ),
-      body: Center()
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: kBackground,
+          elevation: 0,
+          centerTitle: false,
+          title: const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              "Hello Rudiansyah",
+              style: TextStyle(fontSize: 16.0, color: kFontLight),
+            ),
+          ),
+          actions: [
+            Stack(
+              children: const [
+                Icon(
+                  Icons.notifications,
+                ),
+              ],
+            )
+          ],
+        ),
+        body: const Center());
   }
 }
