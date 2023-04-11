81fd8459eb3538e8980ebfe4e06e1c557ea25809
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-04-11 15:59:06 +0700

    feat: create home_screen.dart

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      // appBar: AppBar(
+      //   title: const Text("Dashboard"),
+      //   actions: const [],
+      // ),
+      body: Center()
+    );
+  }
+}
