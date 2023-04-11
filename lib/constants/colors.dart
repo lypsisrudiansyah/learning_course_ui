8cc71d526607e574c975c96aa88e825ae67c588e
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-04-11 16:00:50 +0700

    update colors.dart and main.dart

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,3 +5,6 @@ const Color kBackground = Color(0xFFFCFCFC);
 const Color kPrimaryLight = Color(0xFFEDF6F3);
 const Color kPrimary = Color(0xFFDAEFE8);
 const Color kPrimaryDark = Color(0xFF88A1AE);
+const Color kAccent = Color(0xFFFDCA73);
+const Color kFont = Color(0xFF252435);
+const Color kFontLight = Color(0xFFB4B7BF);
