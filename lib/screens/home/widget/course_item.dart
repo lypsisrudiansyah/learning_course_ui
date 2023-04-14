fbc5adbcf627509b81b1701d35d27f21e291371f
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-04-14 20:08:50 +0700

    update course_item.dart

--- lib/screens/home/widget/course_item.dart
+++ lib/screens/home/widget/course_item.dart
@@ -34,7 +34,58 @@ class CourseItem extends StatelessWidget {
                     ),
                   ),
                 ),
-                
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              course.authorImg,
+                              width: 20.0,
+                            ),
+                            const SizedBox(width: 5),
+                            Text(
+                              course.author,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              course.title,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0,
+                                color: kFont,
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 5),
+                              width: 5,
+                              height: 5,
+                              decoration: const BoxDecoration(color: kFontLight, shape: BoxShape.circle),
+                            ),
+                            const Text(
+                              "3h 55min",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14.0,
+                                color: kFont,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
               ],
             ),
           )
